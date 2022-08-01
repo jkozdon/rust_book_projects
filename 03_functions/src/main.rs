e574a56fb827f26e92f68f539b5b043e80393632
Jeremy E Kozdon <jekozdon@nps.edu>
2022-07-31 20:17:10 -0700

    Functions

--- 03_functions/src/main.rs
+++ 03_functions/src/main.rs
@@ -1,9 +1,13 @@
 fn main() {
     println!("Hello, world!");
 
-    another_function();
+    another_function(fives().0);
 }
 
-fn another_function() {
-    println!("Another function.");
+fn another_function(x : i32) {
+    println!("Another function. {x}");
+}
+
+fn fives() -> (i32, i32) {
+    (5, 5)
 }
